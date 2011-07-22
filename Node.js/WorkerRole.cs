498efaa4ac125b0c42d6410cdaed08375cd8c3ff
Andy <andy@sharpcloud.com>
2011-07-22 12:29:53 +0100

    Working example on both dev fabric and on the Azure cloud

---
+++ Node.js/WorkerRole.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace Node.js
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        public override void Run()
+        {            
+            // This is a sample worker implementation. Replace with your logic.
+            Trace.WriteLine("Node.js entry point called", "Information");            
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Trace.WriteLine("Working", "Information");
+            }
+        }
+
+        private void StartNode()
+        {
+            var port = RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["httpIn"].IPEndpoint.Port;
+            var ip = RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["httpIn"].IPEndpoint.Address.ToString();
+            ProcessStartInfo startInfo = new ProcessStartInfo("node.exe", "app.js");
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.EnvironmentVariables.Add("AZURE_PORT", port.ToString());
+            startInfo.EnvironmentVariables.Add("AZURE_IP", ip);
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            Trace.WriteLine(">>>> Assigned port:" + port);
+            try
+            {
+                Process exeProcess = new Process();
+                exeProcess.StartInfo = startInfo;
+                exeProcess.ErrorDataReceived += (sender, evt) => Trace.WriteLine(evt.Data);
+                exeProcess.OutputDataReceived += (sender, evt) => Trace.WriteLine(evt.Data);
+                exeProcess.Start();
+                exeProcess.BeginErrorReadLine();
+                exeProcess.BeginOutputReadLine();
+            }
+            catch (Exception ex)
+            {
+                // Log error.
+                Trace.TraceError(ex.Message);
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            StartNode();
+            return base.OnStart();
+        }
+    }
+}
---
+++ Node.js/app.js
@@ -0,0 +1,10 @@
+﻿var http = require('http');
+var port = process.env.AZURE_PORT;
+var ip = process.env.AZURE_IP;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is node.js v0.5.1 running natively on Windows Azure. Cool eh!\n');
+}).listen(port, ip);
+
+console.log('Server running at ' + ip + ':' + port);
